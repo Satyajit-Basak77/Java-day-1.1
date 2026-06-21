0f4d8103c7188288594e0919c86e11113714f722
Satyajit-Basak77 <satyajitbasak52+github@gmail.com>
2026-06-22 01:36:55 +0530

    input taking from terminal itself

---
+++ takinginput.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+public class takinginput {
+    public static void main(String[] args) {
+        //inputs
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your name please:");
+        String name = sc.nextLine();
+        System.out.print("Your name is ");
+        System.out.print(name);
+    }
+}
